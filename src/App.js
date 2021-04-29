f09bfa2af22ada687e971eb8a9bcb19c6977a0aa
David Bennett <dbennett455@Johns-MacBook-Pro.local>
2021-04-29 09:38:00 -0500

    Remove external data from repo.
    This data is downloaded automatically from the
    util/filter_us.sh script.

--- src/App.js
+++ src/App.js
@@ -28,14 +28,32 @@ class App extends React.Component {
   }
 
   weatherCall() {
+    let apiurl=null;
+    if (process.env.NODE_ENV == 'development') {
+      // run with proxy to avoid CORS issues
+      apiurl="http://localhost:3000/data/2.5";
+    } else {
+      apiurl="https://api.openweathermap.org/data/2.5";
+    }
     let openWeatherMap = new OpenWeatherMap({
-      base_url: "http://localhost:3000/data/2.5",
+      base_url: apiurl,
       api_key: "4ff9086409bab044f46489691f1c0d96"
     }); 
     openWeatherMap.getWeather({q: this.state.city, units: this.state.units}).then(result => {
-      console.log(result);
-      return result;
+      if (typeof(result) === 'object' 
+          && typeof(result.body) === 'object' 
+          && result.body.temp > -999) {
+            this.state.results=result.body;
+            this.state.error=null;
+          } else {
+            this.state.results=null;
+            this.state.error="City "+this.state.city+" not found";
+          }
+          console.log(result);
+          return;
     }).catch((err) => {
+      this.state.result=null;
+      this.state.error="City "+this.state.city+" not found";
       console.log(err);
     });
   }
