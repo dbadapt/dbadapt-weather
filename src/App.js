207da08a0b141137e36d74af6faf2e889307494d
David Bennett <dbennett455@Johns-MacBook-Pro.local>
2021-04-28 22:56:38 -0500

    Added error handling to API call

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component {
     super(props);
     this.state = {
         city: null,
-        units: "imperial"
+        units: "imperial",
+        results: null,
+        error: null
     }
     this.weatherCall = this.weatherCall.bind(this);
   } 
@@ -32,6 +34,9 @@ class App extends React.Component {
     }); 
     openWeatherMap.getWeather({q: this.state.city, units: this.state.units}).then(result => {
       console.log(result);
+      return result;
+    }).catch((err) => {
+      console.log(err);
     });
   }
 
